57d0a4a252aad7a8452ab4b9ee80c8127ecb0c45
Johannes Kühnel <johannes.kuehnel@t-systems.com>
2020-06-25 23:50:45 +0200

    unify into one dockerfile

---
+++ selenium/chromium/build-83.0.4103.61-0ubuntu0.18.04.1.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./build.sh 83.0.4103.61-0ubuntu0.18.04.1
---
+++ selenium/chromium/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+if [[ -z "$1" ]]; then
+    echo "please provide 1 argument : chromium-browser ubuntu package version tag"
+    exit 2
+fi
+
+docker build -t selenoid/chromium:${VERSION} .
